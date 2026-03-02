5db68620e05465104f7601b6401effe4837f9464
Jobulikannan <kannan@jobuli.com>
2026-03-02 17:14:14 +0530

    connect-to-frontend completed on
    02.03.2026

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
